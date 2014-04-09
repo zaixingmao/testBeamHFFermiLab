ee5ff042b41d4f28d7affa0d2c74c5243e358598
Zaixing Mao <zaixing.mao@cern.ch>
2014-04-09 15:17:23 -0500

    update

--- startUp_RunMe.py
+++ startUp_RunMe.py
@@ -12,30 +12,24 @@ import json
 #     os.system('su -c \'/opt/cactus/bin/controlhub_start\'')
 # 
 # 
-# #source stuff!
-subprocess.check_call('''source ~daqowner/daq.11.11.4/etc/env.csh; python -c "import os, json; print(json.dumps(dict(os.environ)))"''',
-                       shell=True, executable='/bin/csh')
-#subprocess.check_call(['/bin/csh', '-c', 'source ~daqowner/daq.11.11.4/etc/env.csh'])
-subprocess.check_call(['/bin/csh', '-c', 'source /home/hcalpro/seema/TTTSoftware/environ.csh'])
-subprocess.check_call(['/bin/csh', '-c', 'source ~hcalpro/sckao/11_11_4/hcal/hcalUHTR/source.csh'])
-
 #configure TTT to take external clock
-os.system('./externalClockTTT.sh | python ~hcalpro/seema/TTTSoftware/src/TTTtool.py')
+os.system('externalClockTTT.sh | python ~hcalpro/seema/TTTSoftware/src/TTTtool.py')
  
 # #make AMC13 distribute the clock
-am13_command = "AMC13Tool.exe -u -n 11 -x AMC13Com.txt > log.txt" 
+am13_command = 'AMC13Tool.exe -u -n 11 -x AMC13Com.txt > log.txt'
 os.system(am13_command)
 # 
 #  
 # #configure ngCCM to take external clock
-# #os.system('cd ~/GLIBtool_UVA/')
+os.chdir('~/GLIBtool_UVA/')
 # # ./GLIBtool
-# os.system('GLIB.sh | ./../GLIBtool_UVA/GLIBtool > log.txt')
+currentDir = os.getcwd()
+os.system('%s/GLIB.sh | ./GLIBtool > log.txt' %currentDir)
 # # cd -
 # 
-# #make uHTR run correct clock
-# uHTR_command =  "bin/linux/x86_64_slc6/uHTRtool.exe 192.168.114.16 -s init.uhtr > log.txt"(htr_tool,i)
-# os.system(daq_check_command)
+#make uHTR run correct clock
+uHTR_command = 'bin/linux/x86_64_slc6/uHTRtool.exe 192.168.114.16 -s init.uhtr > log.txt'
+os.system(uHTR_command)
 
 # 
 
